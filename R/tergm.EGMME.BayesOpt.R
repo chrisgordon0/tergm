71ee4c7aa101fe148bb267361decb3ad86e181d7
Chris <chrisgordon101@gmail.com>
2023-05-22 15:32:20 +1000

    added basic MCMC sampling

--- R/tergm.EGMME.BayesOpt.R
+++ R/tergm.EGMME.BayesOpt.R
@@ -5,21 +5,36 @@
 # Calculate the mahalanobis distance/cost function (this should be done in GD so can copy)
 # Repeat
 
-tergm.EGMME.GD <- function(theta0, nw, model, model.mon, control, proposal, verbose=FALSE){
+tergm.EGMME.bayesOpt <- function(theta0, nw, model, model.mon, control, proposal, verbose=FALSE){
+  
+  theta <- theta0
+  
+  # this is where we combine models and pad out eta 
+  # with 0s as necessary to accomodate the monitoring model
+  model.comb <- c(model, model.mon)
+  proposal$aux.slots <- model.comb$slots.extra.aux$proposal
+  
+  # always collect if monitoring model is passed
+  control$collect <- TRUE
+  control$changes <- FALSE
+  
+  control$time.burnin <- 1
+  control$time.interval <- 1
+  control$time.samplesize <- 1
 
-states <- replicate(nthreads(control),
-            {
-              list(nw=nw,
-                   eta = ergm.eta(theta0, model$etamap),
-                   nw.diff  = nw.diff)
-            },
-            simplify=FALSE
-)
-
-
-out <- parallel::clusterApply(ergm.getCluster(control), seq_along(states), function(i) tergm_MCMC_sample(states[[i]]$nw, model, model.mon, proposal, eta=states[[i]]$eta, control=control.phase1, verbose=verbose))
-
-print(out)
-
-
+  
+  current_ergm_state <- ergm_state(nw, model=model.comb, proposal=proposal,
+                                   stats=c(numeric(model$etamap$etalength), model.mon$nw.stats-model.mon$target.stats))
+  
+  # call many times
+  # re-run every time update theta
+  eta <- ergm.eta(theta, model$etamap)
+  eta.comb <- c(eta, numeric(model.mon$etamap$etalength))
+  # browser()
+  z <- tergm_MCMC_slave(current_ergm_state, eta.comb, control, verbose)
+  
+  #sometging like this... z.stats$matrix
+  # stats should already be centred so no need to do subtraction
+  
+  # cost function takes theta, map onto eta, call mcmc slave, get statistics, get mahanob distance
 }
