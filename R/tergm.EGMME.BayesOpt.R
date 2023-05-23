b1a80e9a6240881e7ed137d85e4e8ee9fad5e57c
Chris <chrisgordon101@gmail.com>
2023-05-23 14:50:06 +1000

    Added beginning functionality for bayes optimization. Not yet tested.

--- R/tergm.EGMME.BayesOpt.R
+++ R/tergm.EGMME.BayesOpt.R
@@ -8,33 +8,90 @@
 tergm.EGMME.bayesOpt <- function(theta0, nw, model, model.mon, control, proposal, verbose=FALSE){
   
   theta <- theta0
-  
+  #print(theta)
+
   # this is where we combine models and pad out eta 
-  # with 0s as necessary to accomodate the monitoring model
+  # with 0s as necessary to accommodate the monitoring model
   model.comb <- c(model, model.mon)
   proposal$aux.slots <- model.comb$slots.extra.aux$proposal
-  
+
   # always collect if monitoring model is passed
   control$collect <- TRUE
   control$changes <- FALSE
   
-  control$time.burnin <- 1
-  control$time.interval <- 1
-  control$time.samplesize <- 1
+  control$time.burnin <- 10
+  control$time.interval <- 10
+  control$time.samplesize <- 10
 
   
-  current_ergm_state <- ergm_state(nw, model=model.comb, proposal=proposal,
-                                   stats=c(numeric(model$etamap$etalength), model.mon$nw.stats-model.mon$target.stats))
+  # Must assign globals here, and after the optimization I should clean them up
+  # and remove them from the environment.
+  # The reason I must make them globals is because the cost function for
+  # Bayesian Optimization can only have one parameter.
+  global.control <<- control
+  global.theta <<- theta
+  global.nw <<- nw
+  global.model <<- model
+  global.model.comb <<- model.comb
+  global.model.mon <<- model.mon
+  global.proposal <<- proposal
+  global.verbose <<- verbose
+  
+  # Need to do this as the cost function cannot be a vector
+  # (limitation in the rBayesianOptimization package)
+  # TODO: Maybe change which optimization package we use.
+  bounds <- list()
+  bounds_length <- length(theta)
+  
+  for (i in 1:vector_length) {
+    bounds[[paste0("x", i)]] <- c(-1, 1)
+  }
+  
+  optim_result <- BayesianOptimization(optimCostFunctionWrapper,
+                                       bounds = bounds,
+                                       init_points = 2, n_iter = 2,
+                                       acq = "ucb", kappa = 1, eps = 0.0,
+                                       verbose = TRUE)
+  
+  optimal_inputs <- optim_result$Best_Par
+  optimal_value <- optim_result$Best_Value
+  
+  print(optimal_inputs)
+  print(optimal_value)
+
+}
+
+mahalanobisDist <- function(target_statistics) {
+  cov_matrix <- cov(target_statistics)
+  inv_cov_matrix <- solve(cov_matrix)
+  
+  dist <- sqrt(mahalanobis(target_statistics, rep(0, ncol(target_statistics)), inv_cov_matrix))
+  
+  return(dist)
+}
+
+optimCostFunction <- function(theta) {
+  
+  current_ergm_state <- ergm_state(global.nw, model=global.model.comb, proposal=global.proposal,
+                                   stats=c(numeric(global.model$etamap$etalength), global.model.mon$nw.stats - global.model.mon$target.stats))
   
   # call many times
   # re-run every time update theta
-  eta <- ergm.eta(theta, model$etamap)
-  eta.comb <- c(eta, numeric(model.mon$etamap$etalength))
-  # browser()
-  z <- tergm_MCMC_slave(current_ergm_state, eta.comb, control, verbose)
+  eta <- ergm.eta(theta, global.model$etamap)
+  eta.comb <- c(eta, numeric(global.model.mon$etamap$etalength))
+
+  z <- tergm_MCMC_slave(current_ergm_state, eta.comb, global.control, global.verbose)
   
-  #sometging like this... z.stats$matrix
-  # stats should already be centred so no need to do subtraction
+  # override ergm state - why?
+  current_ergm_state <- z$state
   
-  # cost function takes theta, map onto eta, call mcmc slave, get statistics, get mahanob distance
+  target_statistics <- z$statsmatrix
+  
+  return(list(Score = mahalanobisDist(target_statistics), Pred = 0))  
 }
+
+optimCostFunctionWrapper <- function(...) {
+  theta <- c(...)
+  return(optimCostFunction(theta))
+}
+
