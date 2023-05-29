988cf4f4db7a15f9fb4de245d111ed5edabe2915
Chris <chrisgordon101@gmail.com>
2023-05-29 18:50:28 +1000

    Changed singular matrix check to use machine epsilon. Added code to remove globals after optimization is done. Returns a list with relevant data (not finished).

--- R/tergm.EGMME.BayesOpt.R
+++ R/tergm.EGMME.BayesOpt.R
@@ -18,22 +18,19 @@ tergm.EGMME.bayesOpt <- function(theta0, nw, model, model.mon, control, proposal
   
   control$time.burnin <- 1
   control$time.interval <- 1
-  control$time.samplesize <- 100
-
+  control$time.samplesize <- 10
   
   # Must assign globals here, and after the optimization I should clean them up
   # and remove them from the environment.
   # The reason I must make them globals is because the cost function for
-  # Bayesian Optimization can only have one parameter.
+  # Bayesian Optimization can't have helper parameters (i think)
   global.control <<- control
-  global.theta <<- theta0
   global.nw <<- nw
   global.model <<- model
   global.model.comb <<- model.comb
   global.model.mon <<- model.mon
   global.proposal <<- proposal
   global.verbose <<- verbose
-  
   
   obj.fun <- makeSingleObjectiveFunction(
     name = "optimCostFunction",
@@ -44,39 +41,67 @@ tergm.EGMME.bayesOpt <- function(theta0, nw, model, model.mon, control, proposal
     minimize = TRUE
   )
   
-  des = generateDesign(n = 100, par.set = getParamSet(obj.fun), fun = lhs::randomLHS)
+  des = generateDesign(n = 5, par.set = getParamSet(obj.fun), fun = lhs::randomLHS)
   
   des$y = apply(des, 1, obj.fun)
   
   surr.km = makeLearner("regr.km", predict.type = "se", covtype = "matern3_2", control = list(trace = FALSE))
   
   mboControl = makeMBOControl()
-  mboControl = setMBOControlTermination(mboControl, iters = 100)
+  mboControl = setMBOControlTermination(mboControl, iters = 10)
   mboControl = setMBOControlInfill(mboControl, crit = makeMBOInfillCritEI())
   
   run = mbo(obj.fun, design = des, learner = surr.km, control = mboControl, show.info = TRUE)
   
+  # remove later?
   print(run)
     
-  # At the end do something like this to clean up:
-  # rm(global.control) ... etc
+  newnetwork <- as.network(
+                  ergm_state(global.nw, model=global.model.comb, proposal=global.proposal,
+                             stats=c(numeric(global.model$etamap$etalength), 
+                             global.model.mon$nw.stats - global.model.mon$target.stats))
+                )
   
-  # TODO: need to return something
+  theta <- unlist(run$x)
+  eta <- ergm.eta(theta, global.model$etamap)
+  
+  # remove all of the global from the environment
+  rm(global.control)
+  rm(global.nw)
+  rm(global.model)
+  rm(global.model.comb)
+  rm(global.model.mon)
+  rm(global.proposal)
+  rm(global.verbose)
+    
+  return(
+    list(#nw.diff=z$nw.diff,
+         newnetwork=newnetwork,
+         eta=eta
+         #history as well
+    )
+  )
+  
 }
 
 mahalanobisDist <- function(target_statistics) {
   
   cov_matrix <- cov(target_statistics)
-
-  if (rcond(cov_matrix) < 0.01) {
-    return(10000)
+  
+  # check for singular matrix
+  if (rcond(cov_matrix) < .Machine$double.eps) {
+    return(10^13)
   }
   
-  cov <<- cov_matrix
+  cond <<- rcond(cov_matrix)
   
+  # debugging
+  cov <<- cov_matrix
+
   inv_cov_matrix <- solve(cov_matrix)
   
-  inv <<- inv_cov_matrix
+  # debugging
+  #inv <<- inv_cov_matrix
   
   dist <- sqrt(mahalanobis(colMeans(target_statistics), rep(0, ncol(target_statistics)), inv_cov_matrix))
   return(dist)
@@ -102,7 +127,7 @@ optimCostFunction <- function(theta) {
   targets <<- target_statistics
   
   # For debugging
-  mahalanob <<- mahalanobisDist(target_statistics)
+  # mahalanob <<- mahalanobisDist(target_statistics)
   
   return(mahalanobisDist(target_statistics))  
 }
